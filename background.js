958448005783ec5f6b6138af497d1e86e02f1235
karthic <karthic@izonewe.com>
2023-08-14 14:58:10 -0500

    first commit

---
+++ background.js
@@ -0,0 +1,89 @@
+
+let settings = {}
+chrome.storage.sync.get({ gradients: true, controls: true, auto: false }, (items) => {
+	settings = items;
+	})
+
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+	for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+		settings[key] = newValue
+		}
+	});
+
+
+
+function reddenPage(settings) {
+	if (window.location.hostname.toLowerCase().includes("amazon.com")) {
+		if (settings.controls) {
+			const style = document.createElement('style');
+			style.innerHTML = `.webPlayerUIContainer {display: none;}`;
+			document.body.appendChild(style);
+
+			} else if (settings.gradients) {
+			const style = document.createElement('style');
+			style.innerHTML = `.fkpovp9 {display: none;}`;
+			document.body.appendChild(style);
+			}
+		} else if (window.location.hostname.toLowerCase().includes("max.com")) {
+		if (settings.controls) {
+			const style = document.createElement('style');
+			style.innerHTML = `div[class^='AutohiderContainer'] {display: none;}`;
+			document.body.appendChild(style);
+
+			} else if (settings.gradients) {
+			const style = document.createElement('style');
+			style.innerHTML = `div[class^='ProtectionLayerContainer'] {display: none;}`;
+			document.body.appendChild(style);
+			}
+		} else if (window.location.hostname.toLowerCase().includes("netflix.com")) {
+		if (settings.controls) {
+			const style = document.createElement('style');
+			style.innerHTML = `.ltr-16tr625 {display: none;}`;
+			document.body.appendChild(style);
+			} else if (settings.gradients) {
+			const style = document.createElement('style');
+			style.innerHTML = `.watch-video--back-container {display: none;}
+					.watch-video--flag-container {display: none;}`;
+			document.body.appendChild(style);
+			}
+		} else if (window.location.hostname.toLowerCase().includes("mgmplus.com")) {
+		if (settings.controls) {
+			const style = document.createElement('style');
+			style.innerHTML = `.bmpui-ui-uicontainer {opacity: 0;}`;
+			document.body.appendChild(style);
+			} else if (settings.gradients) {
+			const style = document.createElement('style');
+			style.innerHTML = `.bmpui-ui-uicontainer {opacity: 0;}`;
+			document.body.appendChild(style);
+			}
+
+		} else {
+		}
+
+	}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
+	if (settings.auto) {
+		if (changeInfo.status == "complete") {
+			const url = new URL(tab.url);
+			if (url.hostname.toLowerCase().includes("mgmplus.com") || url.hostname.toLowerCase().includes("amazon.com") || url.hostname.toLowerCase().includes("max.com") || url.hostname.toLowerCase().includes("netflix.com")) {
+				chrome.scripting.executeScript({
+					target: { tabId: tab.id },
+					function: reddenPage, 
+					args : [ settings]
+					});
+
+				}
+			}
+		}
+	})
+
+
+chrome.action.onClicked.addListener((tab) => {
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: reddenPage, 
+		args : [ settings]
+		});
+	}); 
---
+++ options.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+
+	chrome.storage.sync.get({ gradients: true, controls: true, auto: false }, (items) => {
+		$("body").append($("<form>").css({"margin":"20px"})
+			.append($("<div>").addClass("mb-3 form-check")
+				.append($("<input>").attr({"type":"checkbox", "id":"gradients"}).addClass("form-check-input").prop({"checked":items.gradients}).on("change", function() {
+					items.gradients = $(this).is(":checked")
+					items.controls = !$(this).is(":checked")
+console.log(items)
+					$("#controls").prop({"checked":items.controls})
+					chrome.storage.sync.set(items);
+
+
+					}))
+				.append($("<label>").attr({}).addClass("form-check-label").html("Only Gradients"))
+				)
+			.append($("<div>").addClass("mb-3 form-check")
+				.append($("<input>").attr({"type":"checkbox", "id":"controls"}).addClass("form-check-input").prop({"checked":items.controls}).on("change", function() {
+					items.controls = $(this).is(":checked")
+					items.gradients = !$(this).is(":checked")
+console.log(items)
+					$("#gradients").prop({"checked":items.gradients})
+					chrome.storage.sync.set(items);
+					}))
+				.append($("<label>").attr({}).addClass("form-check-label").html("Controls and Gradients"))
+				)
+			.append($("<div>").addClass("mb-3 form-check")
+				.append($("<input>").attr({"type":"checkbox", "id":"auto"}).addClass("form-check-input").prop({"checked":items.auto}).on("change", function() {
+					items.auto = $(this).is(":checked")
+console.log(items)
+					chrome.storage.sync.set(items);
+					}))
+				.append($("<label>").attr({}).addClass("form-check-label").html("Automatically Detect"))
+				)
+			)
+		})
+	})
+
+
